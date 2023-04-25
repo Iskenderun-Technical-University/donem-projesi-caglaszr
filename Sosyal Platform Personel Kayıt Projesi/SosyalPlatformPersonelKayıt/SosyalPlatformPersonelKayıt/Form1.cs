5512460948c1c61cc3532db949d9ba130d7e6745
caglaszr <115402312+caglaszr@users.noreply.github.com>
2023-04-25 16:29:58 +0300

    Add files via upload

---
+++ Sosyal Platform Personel Kayıt Projesi/SosyalPlatformPersonelKayıt/SosyalPlatformPersonelKayıt/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace SosyalPlatformPersonelKayıt
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection baglanti = new SqlConnection("Data Source=DESKTOP-CFOCL17\\SQLEXPRESS;Initial Catalog=PersonelKyt;Integrated Security=True");
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'personelKytDataSet.Tbl_Personel' table. You can move, or remove it, as needed.
+            this.tbl_PersonelTableAdapter.Fill(this.personelKytDataSet.Tbl_Personel);
+
+        }
+
+        private void btnlistele_Click(object sender, EventArgs e)
+        {
+            this.tbl_PersonelTableAdapter.Fill(this.personelKytDataSet.Tbl_Personel);
+        }
+
+        private void btnkaydet_Click(object sender, EventArgs e)
+        {
+            baglanti.Open();
+            SqlCommand komut = new SqlCommand("insert into Tbl_Personel(Perad,PerSoyad,PerSehir,PerMaas,PerMeslek) values (@p1,@p2,@p3,@p4,@p5)", baglanti);
+            komut.Parameters.AddWithValue("@p1",Txtad.Text);
+            komut.Parameters.AddWithValue("@p2",Txtsoyad.Text);
+            komut.Parameters.AddWithValue("@p3",Cmbsehir.Text);
+            komut.Parameters.AddWithValue("@p4",Mskmaas.Text);
+            komut.Parameters.AddWithValue("@p5",Txtmeslek.Text);
+            komut.ExecuteNonQuery();
+            baglanti.Close();
+            MessageBox.Show("Personel Eklendi");
+
+        }
+    }
+}
